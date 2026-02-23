1ea053b15226e1774135a10a29c0d8d593e2dc47
MateusDio <165714347+MateusDio@users.noreply.github.com>
2026-02-23 17:34:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-const cards = document.querySelectorAll(".card");
+const cards = document.querySelectorAll("#sobre .card");
 const imagem = document.getElementById("imgDinamica");
+const cima = document.getElementById("cima");
+const scrollsuave = document.querySelector(".scroll-suave");
+
+      
+    cima.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+    cima.addEventListener("mouseover", () => {
+        cima.style.opacity = "0.8";
+    });
+    cima.addEventListener("mouseout", () => {
+        cima.style.opacity = "1";
+    });
+
+    window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        cima.style.display = "block";
+    } else {
+        cima.style.display = "none";
+    }
+});
+
 
 cards.forEach(card => {
     card.addEventListener("click", () => {
-        
-        // Remove classe ativa de todos
+
         cards.forEach(c => c.classList.remove("active"));
 
-        // Adiciona ao clicado
         card.classList.add("active");
 
-        // Troca a imagem
         const novaImagem = card.getAttribute("data-img");
-        imagem.src = novaImagem;
+
+        if (novaImagem) {
+            imagem.src = novaImagem;
+        }
     });
 });
